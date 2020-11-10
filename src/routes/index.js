91e848b1d296fe1f1a2aa329920b6000a152c80d
Ebuka Umeokonkwo <ebukaume@gmail.com>
2020-11-10 17:52:30 +0100

    add documentation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
+const swaggerUi = require('swagger-ui-express');
 const { notFound, ok } = require('../utils/response');
 const { recordRouter } = require('./record');
+const swaggerDocument = require('../../swagger.json');
 
 const API_PREFIX = '/api';
 
@@ -7,7 +9,8 @@ const bindRoutes = (app) => {
   app.use(`${API_PREFIX}/record/`, recordRouter);
   app.use(`${API_PREFIX}/status`, (req, res) => ok(res, { status: 'Online' }));
 
-  app.use((req, res) => notFound(res, 'Oops! Route not found. Please check the docs'));
+  app.use(`${API_PREFIX}/docs`, swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  app.use((req, res) => notFound(res, 'Oops! Route not found. Please check the docs @ /api/docs'));
 };
 
 module.exports = {
